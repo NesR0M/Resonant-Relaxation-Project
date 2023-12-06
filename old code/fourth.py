d75d5e621dd1767ff40a3267dba23f1f39238763
Moritz <morsen@live.de>
2023-12-06 16:14:24 +0100

    clean relaxation pyton script added

--- old code/fourth.py
+++ old code/fourth.py
@@ -4,8 +4,6 @@ import pandas as pd
 
 # Get user input
 relaxation_time_minutes = float(input("Enter the total length of relaxation time in minutes: "))
-flow_time_minutes = float(input("Enter the total length of flow time in minutes: "))
-flow_bpm = float(input("Enter the BPM for the flow wave: "))
 modulator_freq = float(input("Enter the frequency of the modulator wave in Hz: "))
 modulation_reduction_percent = float(input("Enter the desired percentage reduction of the modulator frequency: "))
 
@@ -14,7 +12,6 @@ reduction_factor = 1 - (modulation_reduction_percent / 100)
 
 # Convert total time to seconds
 total_time = relaxation_time_minutes * 60
-total_flow_time = flow_time_minutes * 60
 
 # Define the sample rate
 sample_rate = 44100
@@ -48,41 +45,11 @@ waveform = np.sin(phase) * np.sin(2 * np.pi * modulator_freq_relaxation * time)
 # Normalize the waveform to the range [-1, 1]
 waveform = waveform / np.max(np.abs(waveform))
 
-# Now for the flow wave:
-
-# Calculate the rate of the beats in Hz
-beats_per_second = flow_bpm / 60
-pulse_duration = 1 / beats_per_second
-
-# Generate time array for flow time
-flow_time = np.linspace(0, total_flow_time, int(total_flow_time * sample_rate))
-
-# Interpolate the modulator frequency back to original frequency
-modulator_freq_flow = np.linspace(end_modulator_freq_relaxation, modulator_freq, len(flow_time))
-
-# Define the amplitude envelope for each pulse (beat)
-beat_starts = np.floor(flow_time / pulse_duration) * pulse_duration
-times_into_beat = flow_time - beat_starts
-sine_phases = (times_into_beat / pulse_duration) * (2 * np.pi)
-amplitude_envelope = np.sin(sine_phases) * (sine_phases <= np.pi)
-
-# Generate the flow wave with the modulating frequency being interpolated back to the original value
-flow_wave = amplitude_envelope * np.sin(2 * np.pi * np.cumsum(modulator_freq_flow) / sample_rate)
-
-# Normalize the flow waveform to the range [-1, 1]
-flow_wave = flow_wave / np.max(np.abs(flow_wave))
-
-# Concatenate the two waveforms
-complete_waveform = np.concatenate((waveform, flow_wave))
-
-# Normalize the complete waveform to the range [-1, 1]
-complete_waveform = complete_waveform / np.max(np.abs(complete_waveform))
-
 # Convert the complete waveform to 16-bit PCM
-complete_waveform_int16 = np.int16(complete_waveform * 32767)
+waveform_int16 = np.int16(waveform * 32767)
 
 # Write the complete waveform to a .wav file
-filename_complete = f"complete_waveform_{modulator_freq}Hz.wav"
-write(filename_complete, sample_rate, complete_waveform_int16)
+filename_complete = f"waveform_{modulator_freq}Hz.wav"
+write(filename_complete, sample_rate, waveform_int16)
 
-print(f"Complete waveform has been saved as '{filename_complete}'.")
+print(f"The waveform has been saved as '{filename_complete}'.")
